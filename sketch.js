d42e1a8f3584377d6d88eca4b832fccfacc82a40
Bradie-Hawksworth <67816866+Bradie-Hawksworth@users.noreply.github.com>
2022-01-17 11:53:03 +0000

    Update sketch.js
    Added DiceFace assignment implementation.

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
-//The setup() function is called once when the program starts.
-//https://p5js.org/reference/#/p5/setup
 function setup() {
   createCanvas(400, 400);
-  
 }
 
-//Called directly after setup(), the draw() function continuously executes the lines of code contained inside its block until the program is stopped or noLoop() is called.
-//https://p5js.org/reference/#/p5/draw
 function draw() {
   background(220);
-  
-  //Create a square at x:10 y:20 size:50
-  // https://p5js.org/reference/#/p5/square
-  square(20,20,50);
+}
+
+//This is a user defined function for drawing a dice with the value one.
+function diceValueOne(){
+  strokeWeight(1);
+  square(20, 20, 40);
+  strokeWeight(5);
+  point(40,40, 10);
 }
